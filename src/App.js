3934a7323dc3ee26827cffc09934f2d027419e1e
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-07 14:30:10 -0500

    Created CartContext in addition to ProductContext. App.js is wrapped with both context, and provided state to appropriate components. Page is now rendering as expected - add item functionality is working properly.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from "./contexts/ProductContext"
+import CartContext from "./contexts/CartContext"
 
 
 function App() {
@@ -18,13 +19,15 @@ function App() {
 	};
 
 	return (
-		<div className="App">
+		<CartContext.Provider value={cart}>
 			<ProductContext.Provider value={{products, addItem}}>
-				<Navigation cart={cart} />
-				<Route exact path="/" component={Products} />
-				<Route path="/cart" component={ShoppingCart} />
+				<div className="App">
+					<Navigation cart={cart} />
+					<Route exact path="/" component={Products} />
+					<Route path="/cart" component={ShoppingCart} />
+				</div>
 			</ProductContext.Provider>
-		</div>
+		</CartContext.Provider>
 	);
 }
 
